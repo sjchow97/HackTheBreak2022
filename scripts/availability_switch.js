6e5ab551a8f4f5eb67de6c61f45dd58fdf940a37
Brandon Chan <brandonchan45@gmail.com>
2022-03-13 04:59:40 -0700

    changes to ui

--- scripts/availability_switch.js
+++ scripts/availability_switch.js
@@ -3,7 +3,18 @@ let switchToggle = document.getElementById("flexSwitchCheckDefault");
 
 switchToggle.addEventListener("click", (e)=> {
 
-    
     console.log(switchToggle.checked);
 })
 
+function changeAvailabilityState(){
+    var availabilityState = switchToggle.checked;
+    
+    if (availabilityState != null){
+        document.collection("volunteer").doc(user.uid).set({
+            availability: availabilityState
+        })
+    }
+
+}
+
+changeAvailabilityState();
