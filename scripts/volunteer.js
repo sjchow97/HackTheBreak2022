b6385c0bb242b924cef20cf27fb71cd3630f878a
Brandon Chan <brandonchan45@gmail.com>
2022-03-13 03:03:21 -0700

    updated volunteer.js to display name and email from database

--- scripts/volunteer.js
+++ scripts/volunteer.js
@@ -5,8 +5,7 @@ function insertName() {
                 // Do something for the current logged-in user here: 
                 console.log(volunteer.uid);
                 //go to the correct user document by referencing to the user uid
-                currentUser = db.collection("volunteer").doc(user.uid);
-                console.log(user.uid);
+                currentUser = db.collection("volunteer").doc(volunteer.uid);
                 //get the document for current user.
                 currentUser.get()
 			          .then(volunteerDoc => {
@@ -20,4 +19,26 @@ function insertName() {
         });
     }
 insertName();
+
+function insertEmail() {
+    firebase.auth().onAuthStateChanged(volunteer => {
+        // Check if user is signed in:
+        if (volunteer) {                                                                 
+            // Do something for the current logged-in user here: 
+            console.log(volunteer.uid);
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("volunteer").doc(volunteer.uid);
+            //get the document for current user.
+            currentUser.get()
+                  .then(volunteerDoc => {
+               var user_Name = volunteerDoc.data().email;
+               console.log(user_Name);
+               document.getElementById("email-goes-here").innerText = user_Name;   
+            })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+insertEmail();
 
