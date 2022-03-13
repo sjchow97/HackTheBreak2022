b0431c58c957ba2bab69acb4ca779eec2331e39b
Bucksin <bucksin8@gmail.com>
2022-03-13 11:46:42 -0700

    update

--- scripts/user-page2.js
+++ scripts/user-page2.js
@@ -16,6 +16,18 @@ eventList.get()
 
         let name = document.querySelectorAll("#volunteer-name");
         console.log(name[0].innerText);
+
+        for(i=0; i < name.length; i++ ) {
+            name[i].addEventListener("click", (e) => {
+                let url = document.location.href;
+                let newUrl = url.replace("user-page2.html", "user-page3.html" );
+                newUrl = newUrl + ":" + e.currentTarget.innerText;
+                console.log(newUrl);
+
+                document.location.href = newUrl;
+
+            })
+        }
         //let name1 = namebox[0].getElementById("volunteer-name");
         //console.log(name1.innerText);
 
